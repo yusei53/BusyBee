aa4ebdcede79cc207e457d816c950d0ab1bde645
yusei53 <yusei530922@gmail.com>
2024-07-24 18:51:22 +0900

    fix title and text

--- app/sort/page.tsx
+++ app/sort/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import SortTodoPage from "../components/sort/sort-todo-page";
+
+export const metadata: Metadata = {
+  title: "タスク吐き出し場所",
+};
 
 const page = () => {
   return <SortTodoPage />;
